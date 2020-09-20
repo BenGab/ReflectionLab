87c452854e6059973815a362a322fb57aa21b63c
Gabor Benko <benkogabor@msn.com>
2020-09-20 20:20:34 +0200

    created XMlSerializer

---
+++ LABXMLASerializer/Attributes/ExcludeXMLAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace LABXMLASerializer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple =true)]
+    public class ExcludeXMLAttribute : Attribute
+    {
+        public string Reason { get; set; }
+    }
+}
---
+++ LABXMLASerializer/LabXMLSerializer.cs
@@ -0,0 +1,48 @@
+﻿using LABXMLASerializer.Attributes;
+using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace LABXMLASerializer
+{
+    public class LabXMLSerializer
+    {
+        private bool IsAllowed(PropertyInfo property)
+        {
+            return property.GetCustomAttribute<ExcludeXMLAttribute>() == null;
+        }
+
+        private string GetPrettyName(PropertyInfo property)
+        {
+            var attr = property.GetCustomAttribute<DisplayNameAttribute>();
+
+            return attr != null ? attr.DisplayName : property.Name;
+        }
+
+        public XElement ToXML<T>(T instance) where T : class
+        {
+            if (instance == null)
+            {
+                throw new ArgumentException(nameof(instance));
+            }
+
+            XElement node = new XElement("instance");
+            Type instanceType = instance.GetType();
+
+            foreach(PropertyInfo property in instanceType.GetProperties())
+            {
+                if(IsAllowed(property))
+                {
+                    XElement dataNode = new XElement("data");
+                    dataNode.Add(new XAttribute("name", property.Name));
+                    dataNode.Add(new XAttribute("prettyName", GetPrettyName(property)));
+                    dataNode.Value = property.GetValue(instance).ToString();
+                    node.Add(dataNode);
+                }
+            }
+
+            return node;
+        }
+    }
+}
